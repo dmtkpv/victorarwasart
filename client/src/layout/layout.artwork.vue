109487279afedebbb52f2a300bfd050fbd9c7e58
Dmitriy Karpov <dmtkpv@gmail.com>
2021-02-25 22:04:55 +0200

    update artwork layout

--- client/src/layout/layout.artwork.vue
+++ client/src/layout/layout.artwork.vue
@@ -20,77 +20,29 @@
 
 
     // --------------------
-    // Slider
+    // Images
     // --------------------
 
-    .l-artwork-slider {
+    .l-artwork-images {
 
         @extend %u-row;
-        position: relative;
-        overflow: hidden;
-        align-items: stretch;
-        padding-top: 64px;
         flex: 1;
+        padding: 64px 64px 0 64px;
+        justify-content: center;
+        overflow: hidden;
 
-        .area {
-            position: relative;
-            flex: 0 0 60px;
-            z-index: 2;
-            &.disabled { pointer-events: none; }
-        }
-
-        .holder {
-            @extend %u-row;
-            align-items: stretch;
-            flex: 1 0 0;
-        }
-
-        .slide {
-            @extend %u-row;
-            position: relative;
-            align-items: center;
-            justify-content: center;
-            flex: 0 0 100%;
-            padding: 0 30px;
-
-            img {
-                max-width: 100%;
-                max-height: 100%;
-            }
-
-            &.current {
-                position: relative;
-                z-index: 1;
-            }
-
-            &.prev {
-                transform: translateX(50%);
-                img { transform: translateX(-50%); }
-            }
-
-            &.next {
-                transform: translateX(-50%);
-                img { transform: translateX(50%); }
-            }
-
-        }
-
-        .holder,
-        .slide,
-        .slide img {
-            transition: transform .7s;
+        img {
+            max-width: 100%;
+            max-height: 100%;
+            object-fit: contain;
         }
 
         @include sm {
-            padding-top: 0;
-            .area { display: none }
-            .slide {
-                padding: 0;
-                img {
-                    width: 100%;
-                    height: 100%;
-                    object-fit: cover;
-                }
+            padding: 0;
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
             }
         }
 
@@ -137,7 +89,7 @@
             align-items: flex-start;
             justify-content: flex-end;
             background: rgba($black, .8);
-            .nav { display: none }
+            /*.nav { display: none }*/
             &:not(.active) { display: none }
         }
 
@@ -184,6 +136,8 @@
         @include md-xl { display: none }
     }
 
+
+
 </style>
 
 
@@ -198,26 +152,18 @@
 
         <!-- slider -->
 
-        <div class="l-artwork-slider">
-
-            <a class="area" :class="{ disabled: !slide }" @click="slide--" />
-
-            <div class="holder" :style="{ transform: `translateX(-${slide * 100}%)` }">
-                <div class="slide" v-for="(image, index) in images" :key="index" :class="slideClass(index)">
-                    <img
-                        class="u-stretch"
-                        ref="image"
-                        :src="`${baseURL}/assets/${image}`"
-                        @mouseenter="toggleMagnifier(true)"
-                        @mouseleave="toggleMagnifier(false)"
-                        @touchstart.prevent
-                    >
-                </div>
-            </div>
-
-            <a class="area prev" :class="{ disabled: slide === images.length - 1 }" @click="slide++" />
-
+        <div class="l-artwork-images">
+            <img v-for="(image, i) in images"
+                class="u-stretch"
+                ref="image"
+                v-show="i === index"
+                :src="`${baseURL}/assets/${image}`"
+                 @mouseenter="toggleMagnifier(true)"
+                 @mouseleave="toggleMagnifier(false)"
+                 @touchstart.prevent
+            >
         </div>
+
 
 
         <!-- text -->
@@ -225,10 +171,10 @@
         <div class="l-artwork-text" :class="{ active: details }">
 
             <div class="nav" :class="{ hidden: images.length === 1 }">
-                <a v-for="(image, index) in images"
-                   :class="{ active: index === slide }"
-                   @click="slide = index"
-                   v-text="index + 1"
+                <a v-for="(image, i) in images"
+                   :class="{ active: i === index }"
+                   @click="index = i"
+                   v-text="i + 1"
                 />
             </div>
 
@@ -240,10 +186,10 @@
         </div>
 
 
-        <!-- zoom -->
+        <!-- magnifier -->
 
         <div class="l-artwork-magnifier" ref="magnifier" v-show="magnifier">
-            <img :src="`${baseURL}/assets/${images[slide]}`">
+            <img :src="`${baseURL}/assets/${images[index]}`">
         </div>
 
 
@@ -285,7 +231,7 @@
 
         data () {
             return {
-                slide: 0,
+                index: 0,
                 details: false,
                 magnifier: false
             }
@@ -320,7 +266,7 @@
             toggleMagnifier (value) {
                 this.magnifier = value;
                 if (!value) return;
-                const $src = this.$refs.image[this.slide];
+                const $src = this.$refs.image[this.index];
                 const rect = $src.getBoundingClientRect();
                 const pageX = rect.left + rect.width / 2;
                 const pageY = rect.top + rect.height / 2;
@@ -328,45 +274,25 @@
             },
 
             moveMagnifier (e) {
-
                 if (!this.magnifier) return;
-
-                // console.log('moveMagnifier')
-
                 const $node = this.$refs.magnifier;
-                const $src = this.$refs.image[this.slide];
+                const $src = this.$refs.image[this.index];
                 const $dst = $node.firstElementChild;
-
-
                 const event = (e.touches && e.touches[0] || e.changedTouches && e.changedTouches[0] || e);
                 const node = $node.getBoundingClientRect();
                 const src = $src.getBoundingClientRect();
-
-                // console.log(src)
-
                 const nodeX = event.pageX - node.width / 2;
                 const nodeY = event.pageY - node.height / 2;
-
-
                 const dstX = src.left - nodeX;
                 const dstY = src.top - nodeY;
                 const oX = event.pageX - src.left;
                 const oY = event.pageY - src.top;
-
-
-
                 $dst.style.width = src.width + 'px';
                 $dst.style.height = src.height + 'px';
                 $dst.style.transformOrigin = `${oX}px ${oY}px`
                 $dst.style.transform = `translate(${dstX}px, ${dstY}px) scale(2)`
-
-
-
                 $node.style.left = nodeX + 'px';
                 $node.style.top = nodeY + 'px';
-
-
-
             },
 
             inquire () {
