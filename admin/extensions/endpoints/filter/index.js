faba99bac118e381583ad7be2d5be26330d618ef
Dmitriy Karpov <dmtkpv@gmail.com>
2021-03-23 15:52:00 +0200

    fix filter endpoint

--- admin/extensions/endpoints/filter/index.js
+++ admin/extensions/endpoints/filter/index.js
@@ -6,11 +6,12 @@ module.exports = function (router, { database, exceptions }) {
     // Helpers
     // -------------------
 
-    function select ({ table, table_title, tableCount, tableCount_field, tableCount_link }) {
+    function select ({ table, table_title, tableCount, tableCount_field, tableCount_link, tableCheck }) {
         return database.raw(`
             SELECT ${tableCount}.${tableCount_link} as id, ${table}.${table_title}, COUNT(${tableCount_field}) as total
             FROM  ${tableCount}
             INNER JOIN ${table} ON ${tableCount}.${tableCount_link} = ${table}.id
+            where exists (select id from ${tableCheck} where id = ${tableCount}.${tableCount_field})
             GROUP BY ${table}.id;
         `);
     }
@@ -27,7 +28,8 @@ module.exports = function (router, { database, exceptions }) {
             table_title: 'title',
             tableCount: 'artworks_movements',
             tableCount_field: 'artworks_id',
-            tableCount_link: 'artwork_movements_id'
+            tableCount_link: 'artwork_movements_id',
+            tableCheck: 'artworks'
         }).then(records => {
             res.send({ data: records[0] });
         }).catch(next);
@@ -45,7 +47,8 @@ module.exports = function (router, { database, exceptions }) {
             table_title: 'title',
             tableCount: 'artworks_types',
             tableCount_field: 'artworks_id',
-            tableCount_link: 'artwork_types_id'
+            tableCount_link: 'artwork_types_id',
+            tableCheck: 'artworks'
         }).then(records => {
             res.send({ data: records[0] });
         }).catch(next);
@@ -63,7 +66,8 @@ module.exports = function (router, { database, exceptions }) {
             table_title: 'title',
             tableCount: 'publications_types',
             tableCount_field: 'publications_id',
-            tableCount_link: 'publication_types_id '
+            tableCount_link: 'publication_types_id',
+            tableCheck: 'publications'
         }).then(records => {
             res.send({ data: records[0] });
         }).catch(next);
